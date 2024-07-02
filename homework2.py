547f3b5ae8ccf0949c7bff904abc57a617425cc9
ekaterina-es <k.kathy95@mail.ru>
2024-07-02 10:49:18 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,8 @@
+hw_count = 12
+hours_for_hw = 1.5
+course_name = "Python"
+time_for_1_task = hours_for_hw/hw_count
+# Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+print("Курс: ", course_name, ", всего задач: ", hw_count,
+      ", затрачено часов: ", hours_for_hw,
+      ", среднее время выполнения ", time_for_1_task, " часа.", sep='')
